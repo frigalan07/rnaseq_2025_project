eb4d6ea5e7b888a47060dd3d610b4f98ef05f17b
frigalan07 <fridagh@lcg.unam.mx>
2025-02-06 01:23:55 -0600

    Adding progress on the code

--- R/Code.R
+++ R/Code.R
@@ -36,7 +36,7 @@ rse_gene_SRP193734 <- create_rse(project_info)
 ## Convert raw counts to read counts to normalize for gene length and sequencing depth
 assay(rse_gene_SRP193734, "counts") <- compute_read_counts(rse_gene_SRP193734)
 
-## Inspect the attributes of the samples
+## Expand the attributes of the samples
 rse_gene_SRP193734 <- expand_sra_attributes(rse_gene_SRP193734)
 
 colData(rse_gene_SRP193734)[
@@ -44,7 +44,37 @@ colData(rse_gene_SRP193734)[
   grepl("^sra_attribute", colnames(colData(rse_gene_SRP193734)))
 ]
 
-## Use iSEE to visualize the data
-iSEE::iSEE(rse_gene_SRP193734)
+## Inspect the atrributes of the samples
+rse_gene_SRP193734$sra.sample_attributes
+
+## [1] "selected line;;High Drinker|Sex;;male|source_name;;High Drinker_Nucleus Accumbens|tissue;;Dissected Tissue (Brain) - Nucleus Accumbens"
+## [2] "selected line;;High Drinker|Sex;;male|source_name;;High Drinker_Nucleus Accumbens|tissue;;Dissected Tissue (Brain) - Nucleus Accumbens"
+## [3] "selected line;;High Drinker|Sex;;male|source_name;;High Drinker_Nucleus Accumbens|tissue;;Dissected Tissue (Brain) - Nucleus Accumbens"
+## [4] "selected line;;High Drinker|Sex;;male|source_name;;High Drinker_Nucleus Accumbens|tissue;;Dissected Tissue (Brain) - Nucleus Accumbens"
+## [5] "selected line;;High Drinker|Sex;;male|source_name;;High Drinker_Nucleus Accumbens|tissue;;Dissected Tissue (Brain) - Nucleus Accumbens"
+## ...
+
+## Get the data in the columns
+names(colData(rse_gene_SRP193734))
+
+## SELECTING OUR DATA
+## Casting the data of interest (for a better manipulation)
+rse_gene_SRP193734$sra_attribute.selected_line <- factor(rse_gene_SRP193734$sra_attribute.selected_line)
+
+## Check more information about our data
+levels(rse_gene_SRP193734$sra_attribute.selected_line)
+summary(rse_gene_SRP193734$sra_attribute.selected_line)
+
+## As a matter of interest, we will filter the data to only include samples from the selected section of the brain (Nucleus Accumbens)
+rse_gene_SRP193734 <- rse_gene_SRP193734[, rse_gene_SRP193734$sra_attribute.tissue == "Dissected Tissue (Brain) - Nucleus Accumbens"]
+## Casting of the tissue variable
+colData(rse_gene_SRP193734)$sra_attribute.tissue <- factor(colData(rse_gene_SRP193734)$sra_attribute.tissue)
+## Visualize our actual data
+rse_gene_SRP193734$sra.sample_attributes
+
+## Resume of our variable of interest
+summary(as.data.frame(colData(rse_gene_SRP193734)[
+  , grepl("^sra_attribute.*(selected_line|tissu)", colnames(colData(rse_gene_SRP193734)))
+]))
 
 
